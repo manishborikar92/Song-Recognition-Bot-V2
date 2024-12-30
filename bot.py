454dd8c7ebab87777db1458a16782d9daca35a2f
Manish Borikar <manishborikar92@gmail.com>
2024-12-30 16:33:52 +0530

    Version 3.2

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def home():
     return "Bot is running!"
 
 def run_flask():
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("POR", 5000))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == "__main__":
